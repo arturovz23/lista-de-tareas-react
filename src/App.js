5ad1329500bcff252fc82de363d364734743153e
Arturo Valencia <10554086+arturovz23@users.noreply.github.com>
2023-04-23 20:12:45 -0700

    Creacion de la funcionalidad de la aplicacion TODO con react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
 import Header from './componentes/Header'
 import FormularioTareas from './componentes/FormularioTareas';
 import ListaTareas from './componentes/ListaTareas'
 
 function App() {
+  //Obtener las tareas guardadas del localStorage
+  const tareasGuardadas = localStorage.getItem('tareas') ? JSON.parse(localStorage.getItem('tareas')) : [];
+  //Establecemos el estado de las tareas
+  const[tareas, cambiarTareas] = useState(tareasGuardadas);
 
-  const[tareas, cambiarTareas] = useState(
-    [
-      {
-        id: 1,
-        texto: 'Lavar la ropa',
-        completada: false
-      } 
-    ]
-  )
+  //Guardando el estado dentro del localStorage
+  useEffect(() => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+  }, [tareas])
 
-    console.log(tareas);
+  //Accedemos a localStorage y comprobar si mostrarCompletadas es null
+  let configMostrarCompletadas = '';
+  if(localStorage.getItem('mostrarCompletadas') === null){
+    configMostrarCompletadas = true;
+  }else{
+    configMostrarCompletadas = localStorage.getItem('mostrarCompletadas') === true;
+  }
+
+  //El estado de mostrarCompletadas
+  const [mostrarCompletadas, cambiarMostrarCompletadas] = useState(configMostrarCompletadas);
+  
+  useEffect(() => {
+    localStorage.setItem('mostrarCompletadas', mostrarCompletadas.toString());
+  }, [mostrarCompletadas])
 
   return (
     <div className='contenedor'>
-      <Header/>
+      <Header 
+        mostrarCompletadas={mostrarCompletadas} 
+        cambiarMostrarCompletadas={cambiarMostrarCompletadas}/>
       <FormularioTareas tareas={tareas} cambiarTareas={cambiarTareas}/>
-      <ListaTareas tareas={tareas}/>
+      <ListaTareas 
+        tareas={tareas}
+        cambiarTareas={cambiarTareas} 
+        mostrarCompletadas={mostrarCompletadas}
+      />
     </div>
     
 
--- src/componentes/Tarea.jsx
+++ src/componentes/Tarea.jsx
@@ -1,25 +1,56 @@
-import React from 'react';
+//Importaciones del componente
+import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faSquareCheck, faEdit, faTimes} from '@fortawesome/free-solid-svg-icons';
+import {faSquareCheck, faEdit, faTimes, faSquare} from '@fortawesome/free-solid-svg-icons';
 
-const Tarea = ({tarea}) => {
+const Tarea = ({tarea, toggleCompletada, editarTarea, borrarTarea}) => { //Estamos mandando a llamar el objeto de 'tarea, toggleCompletada, editarTarea, borrarTarea'
+
+    //Utilizando useState
+    const [editandoTarea, cambiarEditandoTarea] = useState(false);
+    const [nuevaTarea, cambiarNuevaTarea] = useState(tarea.texto)
+
+    //Utilizamos 'handleSubmint' para que nuestra aplicacion no se actualize constantemente con el preventDefault
+    const handleSubmint = (e) => {
+        e.preventDefault();
+        editarTarea(tarea.id, nuevaTarea);
+        cambiarEditandoTarea(false);
+    }
+
     return ( 
         <li className='lista-tareas__tarea'>
             <FontAwesomeIcon 
-                icon={faSquareCheck} 
+                icon={tarea.completada ? faSquareCheck : faSquare} 
                 className='lista-tareas__icono lista-tareas__icono-check'
+                onClick={() => toggleCompletada(tarea.id)}
             />
             <div className='lista-tareas__texto'>
-                {tarea.texto}
+                {/* Operador ternario -- si editandoTarea es verdadero se muestra formulario, si no muestras solo texto de la tarea */}
+                {editandoTarea ? 
+                <form action="" className='formulario-editar-tarea' onSubmit={handleSubmint}>
+                    <input 
+                        type="text"
+                        className='formulario-editar-tarea__input'
+                        value={nuevaTarea}
+                        onChange={(e) => cambiarNuevaTarea(e.target.value)}
+                    />
+                    <button 
+                        type='submit'
+                        className='formulario-editar-tarea__btn'>Actualizar</button>
+                </form>
+                : tarea.texto
+                }
             </div>
             <div className='lista-tareas__contenedor-botones'>
+                {/*Boton de editar que switchea el valor de 'editandoTarea' */}
                 <FontAwesomeIcon 
                     icon={faEdit} 
                     className='lista-tareas__icono lista-tareas__icono-accion'
+                    onClick={() => cambiarEditandoTarea(!editandoTarea)}
                 />
                 <FontAwesomeIcon 
                     icon={faTimes} 
                     className='lista-tareas__icono lista-tareas__icono-accion'
+                    onClick={() => borrarTarea(tarea.id)}
                 />
             </div>
         </li>
